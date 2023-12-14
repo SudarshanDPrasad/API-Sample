87ce83cd82a52828f16792285deb5530ea0d77a2
sudarshan <sudarshanprasad18@gmail.com>
2023-12-14 17:23:11 +0530

    Removed nodemon

--- index.js
+++ index.js
@@ -1,38 +1,14 @@
-const express = require('express');
-const router = express.Router();
+const express = require("express");
+const app = express();
+const router = require('./route');
 
-router.get("/", (request, response) => {
-    const user = request.query.user;
- 
-    response.send(user + "!");
- });
- 
- const users = [];
- 
- router.post("/user", (request, response) => {
-    const { user } = request.body;
- 
-    users.push({ username: user.username, password: user.password });
- 
-    response.json({ loggedIn: true, stauts: "Loggin success" })
- });
- 
- router.get("/users", (request, response) => {
-    response.json(users);
- })
- 
- router.delete("/delete", (request, response) => {
-    const { username, password } = request.body;
- 
-    const exisitingUser = users.find(u =>
-       u.username === username && u.password === password);
- 
-    if (!exisitingUser) {
-       response.status(401).json({ errorStatus: "Credientals Not found" });
-    }
- 
-    users.splice(users.indexOf(exisitingUser), 1);
-    response.json(users);
- })
 
-module.exports = router;
+// app.use((request, _, next) => {
+//    next()
+// })
+
+app.use(express.json());
+app.use("/api",router);
+app.listen(process.env.PORT || 3000, () => {
+   console.log("Server Listening on PORT 3000");
+});
