f04700012f23b37cfcc82bb93cc2bdf40b1ac802
sudarshan <sudarshanprasad18@gmail.com>
2023-12-19 16:58:24 +0530

    feat: chaning the local host

--- database.js
+++ database.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv'
 dotenv.config
 
 const pool = mysql.createPool({
-    host: '127.0.0.1',
+    host: 'localhost:3306',
     user: 'root',
     password: '9986896968@Sdp',
     database: 'users'
